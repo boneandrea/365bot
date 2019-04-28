c4add300be645634ad1efafc68807275d044f10e
Your Name <you@example.com>
2019-04-28 16:51:58 +0900

    Fix lint config 時報の投げ先を.envに出した

--- classes/Team365Bot.php
+++ classes/Team365Bot.php
@@ -1,4 +1,5 @@
 <?php
+
 namespace Util;
 
 require_once __DIR__.'/../vendor/autoload.php';
@@ -9,102 +10,103 @@ use Monolog\Handler\StreamHandler;
 
 class Team365Bot
 {
-    public $msg; // これをパースする
+	public $msg; // これをパースする
 
-    public function __construct($json)
-    {
-        $this->msg=$json;
+	public function __construct($json)
+	{
+		$this->msg = $json;
 
-        $dotenv = Dotenv::create(__DIR__."/..");
-        $dotenv->load(); //.envが無いとエラーになる
-        $this->log = new Logger('MONOLOG_TEST');
-        //ログレベルをDEBUG（最も低い）に設定
-        $handler = new StreamHandler('./logs/app.log', Logger::DEBUG);
-        $this->log->pushHandler($handler);
-    }
+		$dotenv = Dotenv::create(__DIR__.'/..');
+		$dotenv->load(); //.envが無いとエラーになる
+		$this->log = new Logger('MONOLOG_TEST');
+		//ログレベルをDEBUG（最も低い）に設定
+		$handler = new StreamHandler('./logs/app.log', Logger::DEBUG);
+		$this->log->pushHandler($handler);
+	}
 
-    // なんか考えてリプライする
-    public function reply()
-    {
-        $text=$this->msg["events"][0]["message"]["text"];
-        $this->log->addDebug($text);
+	// なんか考えてリプライする
+	public function reply()
+	{
+		$text = $this->msg['events'][0]['message']['text'];
+		$this->log->addDebug($text);
+		$to = ($this->checkMessageType() === 'user') ? getenv('TO_USER_ID') : getenv('GROUP_ID');
+		$msg = $this->createMessage($text);
+		$this->log->addDebug($msg, ['additional']);
+		if (is_array($msg)) {
+			$ret = $this->push($to, $msg);
+		} elseif (is_string($msg)) {
+			$ret = $this->pushText($to, $msg);
+		}
+	}
 
-        $to= ($this->checkMessageType() === "user") ? getenv("TO_USER_ID"): getenv("GROUP_ID");
+	public function checkMessageType()
+	{
+		return (isset($this->msg['events'][0]['source']['groupId'])) ? 'group' : 'user';
+	}
 
-        $msg=$this->createMessage($text);
-        $this->log->addDebug($msg, ["additional"]);
-        if (is_array($msg)) {
-            $ret=$this->push($to, $msg);
-        } elseif (is_string($msg)) {
-            $ret=$this->pushText($to, $msg);
-        }
-    }
+	public function createMessage($text)
+	{
+		if (preg_match('/別にない/', $text)) {
+			return '当然ですね';
+		} elseif (preg_match('/文句がある/', $text)) {
+			return '綾馬場さんがなんとかしてくれますよ';
+		} elseif (preg_match('/365/', $text)) {
+			return '365日雨の日も晴れの日も薄汚れた居酒屋の片隅で酒を飲むことしか知らない人生の無駄遣いの見本市のような皆さん';
+		} elseif (preg_match('/KR/i', $text)) {
+			$str = file_get_contents('kuri.json');
 
-    public function checkMessageType()
-    {
-        return (isset($this->msg["events"][0]["source"]["groupId"])) ? "group":"user";
-    }
+			return [
+				'type' => 'flex',
+				'altText' => 'This is a Flex Message',
+				'contents' => json_decode($str, true),
+			];
+		}
 
-    public function createMessage($text)
-    {
-        if (preg_match("/別にない/", $text)) {
-            return "当然ですね";
-        } elseif (preg_match("/文句がある/", $text)) {
-            return "綾馬場さんがなんとかしてくれますよ";
-        } elseif (preg_match("/365/", $text)) {
-            return "365日雨の日も晴れの日も薄汚れた居酒屋の片隅で酒を飲むことしか知らない人生の無駄遣いの見本市のような皆さん";
-        } elseif (preg_match("/KR/i", $text)) {
-            $str=file_get_contents("kuri.json");
-            return [
-                "type"=> "flex",
-                "altText"=> "This is a Flex Message",
-                "contents"=>json_decode($str, true)
-            ];
-        }
-        return null;
-    }
+		return null;
+	}
 
-    public function push($to, $json)
-    {
-        $payload = [
-            'to' => $to,
-            'messages' => [$json],
-        ];
+	public function push($to, $json)
+	{
+		$payload = [
+			'to' => $to,
+			'messages' => [$json],
+		];
 
-        return $this->_myPost($payload, getenv('LINE_API_PUSH'));
-    }
+		return $this->_myPost($payload, getenv('LINE_API_PUSH'));
+	}
 
-    public function pushText($to, $text)
-    {
-        $this->push($to, [
-            'type' => 'text',
-            'text' => $text
-        ]);
-    }
+	public function pushText($to, $text)
+	{
+		$this->push($to, [
+			'type' => 'text',
+			'text' => $text,
+		]);
+	}
 
-    public function header()
-    {
-        return [
-            'Content-Type: application/json',
-            'Authorization: Bearer '.getenv('LINE_BOT_ACCESS_TOKEN'),
-        ];
-    }
+	public function header()
+	{
+		return [
+			'Content-Type: application/json',
+			'Authorization: Bearer '.getenv('LINE_BOT_ACCESS_TOKEN'),
+		];
+	}
 
-    public function _myPost($payload, $apiUrl)
-    {
-        $ch = curl_init($apiUrl);
-        $options = [
-            CURLOPT_RETURNTRANSFER => true,
-            CURLOPT_POST => true,
-            CURLOPT_HTTPHEADER => $this->header(),
-            CURLOPT_POSTFIELDS => json_encode($payload),
-        ];
+	public function _myPost($payload, $apiUrl)
+	{
+		$ch = curl_init($apiUrl);
+		$options = [
+			CURLOPT_RETURNTRANSFER => true,
+			CURLOPT_POST => true,
+			CURLOPT_HTTPHEADER => $this->header(),
+			CURLOPT_POSTFIELDS => json_encode($payload),
+		];
 
-        curl_setopt_array($ch, $options);
-        $ret = curl_exec($ch);
-        curl_close($ch);
+		curl_setopt_array($ch, $options);
+		$ret = curl_exec($ch);
+		curl_close($ch);
 
-        $this->log->addDebug($ret, ["additional"]);
-        return $ret;
-    }
+		$this->log->addDebug($ret, ['additional']);
+
+		return $ret;
+	}
 }
--- h.php
+++ h.php
@@ -3,7 +3,6 @@
 require_once __DIR__.'/vendor/autoload.php';
 
 use Util\Team365Bot;
-
 use Dotenv\Dotenv;
 use Monolog\Logger;
 use Monolog\Handler\StreamHandler;
@@ -22,35 +21,35 @@ $log->pushHandler($handler);
 // $log->addError('えらー');
 
 if (0) {
-    foreach ($_SERVER as $name => $value) {
-        //$log->addDebug("$name: $value");
-    }
+	foreach ($_SERVER as $name => $value) {
+		//$log->addDebug("$name: $value");
+	}
 }
 
 // TODO: verify
 function verify_signature($sign)
 {
-    //$log->addDebug("HTTP_X_LINE_SIGNATURE: ".$sign);
+	//$log->addDebug("HTTP_X_LINE_SIGNATURE: ".$sign);
 }
 
-if (isset($_SERVER["HTTP_X_LINE_SIGNATURE"])) {
-    // Webhook
-    verify_signature($_SERVER["HTTP_X_LINE_SIGNATURE"]);
-    $json_string = file_get_contents('php://input'); ##今回のキモ
-    $log->addDebug($json_string);
-    $obj = json_decode($json_string, true);
+if (isset($_SERVER['HTTP_X_LINE_SIGNATURE'])) {
+	// Webhook
+	verify_signature($_SERVER['HTTP_X_LINE_SIGNATURE']);
+	$json_string = file_get_contents('php://input'); //#今回のキモ
+	$log->addDebug($json_string);
+	$obj = json_decode($json_string, true);
 
-    $bot=new Team365Bot($obj);
+	$bot = new Team365Bot($obj);
 
-    $bot->reply();
+	$bot->reply();
 } else {
-    // shell実行、時報
-    $bot=new Team365Bot([]);
-    $dotenv = Dotenv::create(__DIR__);
-    $dotenv->load();
+	// shell実行、時報
+	$bot = new Team365Bot([]);
+	$dotenv = Dotenv::create(__DIR__);
+	$dotenv->load();
 
-    $bot->pushText(
-        getenv("TO_USER_ID"),
-        "飲んでんとはよ帰れ老人共！"
-    );
+	$bot->pushText(
+		getenv('TO_ALARM_CHANNEL'),
+		'飲んでんとはよ帰れ老人共！'
+	);
 }
