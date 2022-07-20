9400935b6aabef02e8c7dfe78230dd1b78f1bada
Your Name <you@example.com>
2022-07-20 09:38:49 +0900

    lint

--- src/classes/SendLine.php
+++ src/classes/SendLine.php
@@ -6,75 +6,82 @@ require_once __DIR__.'/../../vendor/autoload.php';
 
 class SendLine
 {
-	public $log; // これをパースする
+  public $log; // これをパースする
 
-	public function __construct($log)
-	{
-		$this->log = $log;
-	}
+  public function __construct($log)
+  {
+    $this->log = $log;
+  }
 
-	public function push(string $to, array $msg)
-	{
-		$payload = [
-			'to' => $to,
-			'messages' => [$msg],
-		];
+  public function push(string $to, array $msg)
+  {
+    $payload = [
+      'to' => $to,
+      'messages' => [$msg],
+    ];
 
-		return $this->_myPost($payload, getenv('LINE_API_PUSH'));
-	}
+    return $this->_myPost($payload, getenv('LINE_API_PUSH'));
+  }
 
-	public function pushText($to, $text)
-	{
-		return $this->push($to, [
-			'type' => 'text',
-			'text' => $text,
-		]);
-	}
+  public function pushText($to, $text)
+  {
+    return $this->push($to, [
+      'type' => 'text',
+      'text' => $text,
+    ]);
+  }
 
-	public function header()
-	{
-		return [
-			'Content-Type: application/json',
-			'Authorization: Bearer '.getenv('LINE_BOT_ACCESS_TOKEN'),
-		];
-	}
+  public function header()
+  {
+    return [
+      'Content-Type: application/json',
+      'Authorization: Bearer '.getenv('LINE_BOT_ACCESS_TOKEN'),
+    ];
+  }
 
-	public function _myPost($payload, $apiUrl)
-	{
-		$ch = curl_init($apiUrl);
-		$options = [
-			CURLOPT_RETURNTRANSFER => true,
-			CURLOPT_POST => true,
-			CURLOPT_HTTPHEADER => $this->header(),
-			CURLOPT_POSTFIELDS => json_encode($payload),
-		];
+  public function _myPost($payload, $apiUrl)
+  {
+    $ch = curl_init($apiUrl);
+    $options = [
+      CURLOPT_RETURNTRANSFER => true,
+      CURLOPT_POST => true,
+      CURLOPT_HTTPHEADER => $this->header(),
+      CURLOPT_POSTFIELDS => json_encode($payload),
+    ];
 
-		curl_setopt_array($ch, $options);
-		$ret = curl_exec($ch);
-        $httpcode = curl_getinfo($ch, CURLINFO_RESPONSE_CODE);
-		curl_close($ch);
+    curl_setopt_array($ch, $options);
+    $ret = curl_exec($ch);
+    $httpcode = curl_getinfo($ch, CURLINFO_RESPONSE_CODE);
+    curl_close($ch);
 
-        syslog(LOG_DEBUG,$httpcode.":".$ret);
-		return ["status"=>$httpcode, "body"=>$ret];
-	}
+    syslog(LOG_DEBUG,$httpcode.":".$ret);
 
-	public function _myGet($apiUrl)
-	{
-		$ch = curl_init($apiUrl);
-		$options = [
-			CURLOPT_RETURNTRANSFER => true,
-			CURLOPT_HTTPHEADER => $this->header(),
-		];
+    if($httpcode){
+      syslog(LOG_DEBUG, "送信成功");
+    }else{
+      syslog(LOG_DEBUG, "送信失敗");
+    }
 
-		curl_setopt_array($ch, $options);
-		$ret = curl_exec($ch);
-		curl_close($ch);
+    return ["status"=>$httpcode, "body"=>$ret];
+  }
 
-		return $ret;
-	}
+  public function _myGet($apiUrl)
+  {
+    $ch = curl_init($apiUrl);
+    $options = [
+      CURLOPT_RETURNTRANSFER => true,
+      CURLOPT_HTTPHEADER => $this->header(),
+    ];
 
-	public function getProfile($userId, $groupId)
-	{
-		return $this->_myGet("https://api.line.me/v2/bot/group/$groupId/member/$userId");
-	}
+    curl_setopt_array($ch, $options);
+    $ret = curl_exec($ch);
+    curl_close($ch);
+
+    return $ret;
+  }
+
+  public function getProfile($userId, $groupId)
+  {
+    return $this->_myGet("https://api.line.me/v2/bot/group/$groupId/member/$userId");
+  }
 }
