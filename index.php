75b41f64fcb7a60dbea2df88e7ba2e96614fb2b2
boneandrea <bone.andrea.anthrax@gmail.com>
2022-11-25 00:12:39 +0900

    Fix log

--- index.php
+++ index.php
@@ -6,7 +6,7 @@ require_once __DIR__.'/vendor/autoload.php';
 use Util\Main;
 
 function e(mixed $msg){
-    error_log(print_r($msg, true));
+    error_log(print_r($msg, true)."\n");
 }
 
 (new Main())->execute();
