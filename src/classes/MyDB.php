25a4dce55545b0a210d41b06058674625c4f569c
boneandrea <bone.andrea.anthrax@gmail.com>
2022-11-25 14:43:37 +0900

    .

--- src/classes/MyDB.php
+++ src/classes/MyDB.php
@@ -38,7 +38,6 @@ class MyDB
 
 	public function insertDrink($data)
 	{
-        e($data);
 		try {
 			// 挿入（プリペアドステートメント）
 			$stmt = $this->pdo->prepare('INSERT INTO drink(user_id, drink, stamp) VALUES (?, ?, ?)');
